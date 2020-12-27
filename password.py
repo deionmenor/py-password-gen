04e988668342cbea49c4472e5eac9466b20fee28
Deion Menor <deionmenor@gmail.com>
2020-12-27 18:18:09 +0800

    Add prompt for password type

--- password.py
+++ password.py
@@ -1,12 +1,33 @@
-def generatePassword():
-    password = "password123"
+
+def generateMemorablePassword():
+    # do something
+    return ""
+
+def generateRandomPassword():
+    # do something
+    return ""
+
+
+def generatePassword(p):
+    password = ""
+    pwd_type = int(p)
+
+    if pwd_type == 1:
+        password = generateRandomPassword()
+    elif pwd_type ==  2:
+        password = generateMemorablePassword()
+    else:
+        print('Invalid type, generating Random Password by default')
+        password = generateRandomPassword()
     return password
+
 
 while True:
     x = input("Hi! Do you want a new secure password?\n")
     
     if x == "Yes":
-        print(generatePassword())
+        pwd_type = input("(1) Random or (2) memorable?\n")
+        print(generatePassword(pwd_type))
         break
 
     elif x == "No":
